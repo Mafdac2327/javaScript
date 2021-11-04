5b36d2421c18cab960b006e9cb656597c2e7c25b
Mafdac2327 <90983810+Mafdac2327@users.noreply.github.com>
2021-11-04 17:51:59 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,8 @@
+const ano = 2021;
+let nascimento = prompt('Qual teu ano de nascimento?');
+let nome = prompt('Qual seu nome?');
+
+
+let idade = ano - nascimento;
+names = nome;
+alert(`Olá, seu_nome_é ${names} e_você tem ${idade} anos`);
